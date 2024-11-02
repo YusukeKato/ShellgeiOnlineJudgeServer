7ac3e6f1e50da36de172d1a34ddbbcc51da2a8c7
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-11-02 15:23:25 +0900

    debug

--- judge.py
+++ judge.py
@@ -11,14 +11,17 @@ answer_image = args[4]
 output = re.sub('\r', '', output)
 answer = re.sub('\r', '', answer)
 
+judge = "null"
 if ((output == answer or output+"\n" == answer or output+"\n\n" == answer) and output_image == answer_image):
-  print("true")
+  judge = "true"
 else:
-  print("false")
+  judge = "false"
+print(judge)
 
 f = open('debug.txt', 'w', encoding='UTF-8')
-f.write('output: '+output+'\n')
-f.write('answer: '+answer+'\n')
+f.write('output: '+output.replace("\n","<newline>")+'\n')
+f.write('answer: '+answer.replace("\n","<newline>")+'\n')
 f.write('output_image: '+output_image+'\n')
 f.write('answer_image: '+answer_image+'\n')
+f.write('judge: '+judge+'\n')
 f.close()
