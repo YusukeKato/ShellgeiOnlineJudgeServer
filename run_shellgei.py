ffb777bb4c2d0b80bb7611e9154c54356499e4e8
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-11-09 10:57:33 +0900

    環境の依存するライブラリを削除

--- run_shellgei.py
+++ run_shellgei.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-import sys, subprocess
+import sys
+import subprocess
 import asyncio
 import time
-import timeout_decorator
-sys.path.append('/home/ec2-user/.local/lib/python3.9/site-packages/timeout_decorator')
 
-@timeout_decorator.timeout(5)
 async def main(cid):
   try:
     result = await asyncio.wait_for(shellgei(cid), timeout=5)
@@ -13,7 +11,6 @@ async def main(cid):
   except asyncio.TimeoutError:
     print("!!time out!!")
 
-@timeout_decorator.timeout(5)
 async def shellgei(cid):
   result = subprocess.run(['sudo', 'docker', 'exec', cid, '/bin/bash', '-c','./z.bash'], encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   return result.stdout
